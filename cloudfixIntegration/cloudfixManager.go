056ed5394e49ac3f6306eabb32090b2ebe7f2ba4
Arpan Gupta <arpan.gupta@trilogy.com>
2022-12-18 14:55:36 +0530

    Removing extra file creation

--- cloudfixIntegration/cloudfixManager.go
+++ cloudfixIntegration/cloudfixManager.go
@@ -217,12 +217,6 @@ func (c *CloudfixManager) GetReccos() (map[string]map[string][]string, *customEr
 		if errT != nil {
 			return mapping, errT
 		}
-		var responses []ResponseReccos
-		if len(reccos) != 0 {
-			json.Unmarshal(reccos, &responses) //the reccomendations from cloudfix are being unmarshalled
-			file, _ := json.MarshalIndent(responses, "", " ")
-			_ = ioutil.WriteFile("CloudFixReccos.json", file, 0644)
-		}
 	}
 	attrMapping := []byte(`{
 		"Gp2Gp3": {
