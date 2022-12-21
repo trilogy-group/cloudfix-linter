7fe48f93a268ca9f590fcf8184f1ac51f67c1325
Arpan Gupta <arpan.gupta@trilogy.com>
2022-12-21 18:51:25 +0530

    Added chmod statemnts for cloudfi-linter binary

--- install.sh
+++ install.sh
@@ -27,6 +27,7 @@ TERRAFORM_VERSION=1.2.6
 path=$(pwd)
 path+="/terraform"
 alias terraform=$path
+chmod +x terraform
 
 #Installing yor_trace 
 YOR_VERSION=0.1.158
@@ -36,6 +37,7 @@ wget https://github.com/bridgecrewio/yor/releases/download/${YOR_VERSION}/yor_${
 path=$(pwd)
 path+="/yor"
 alias yor=$path
+chmod +x yor
 
 #Installing tflint 
 # higher version have breaking changes to the plugin system and hence we can't install them without changing the plugin
@@ -43,14 +45,18 @@ export TFLINT_VERSION=v0.39.3
 (wget https://github.com/terraform-linters/tflint/releases/download/${TFLINT_VERSION}/tflint_${PLATFORM}.zip \
   && unzip tflint_${PLATFORM}.zip \
   && rm tflint_${PLATFORM}.zip)
+# Setting alias for tflint so that it can be used via command line without referencing the binary path
 path=$(pwd)
 path+="/tflint"
 alias tflint=$path
+chmod +x tflint
 
 # Install cloudfix-linter
 echo "Installing cloudfix-linter"
-(wget https://github.com/trilogy-group/cloudfix-linter-developer/releases/latest/download/cloudfix-linter-developer_${PLATFORM} \
-  && mv cloudfix-linter-developer_${PLATFORM} cloudfix-linter)
+(wget https://github.com/trilogy-group/cloudfix-linter/releases/latest/download/cloudfix-linter_${PLATFORM} \
+  && mv cloudfix-linter_${PLATFORM} cloudfix-linter)
+# Setting alias for cloudfix-linter so that it can be used via command line without referencing the binary path
 path=$(pwd)
 path+="/cloudfix-linter"
 alias cloudfix-linter=$path
+chmod +x cloudfix-linter
